42cc24a5de1d9fcdd57a4f0a2ac8508c70fb866d
ekocakk <eminekocak1994@gmail.com>
2021-05-06 14:28:29 +0300

    Diet App
    Getting nutritional advice based on body mass index.

---
+++ DietApp/DietApp/app/src/main/java/com/example/enes/dietapp/activities/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.enes.dietapp.activities;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.example.enes.dietapp.R;
+
+import com.example.enes.dietapp.fragments.DietFragment;
+import com.example.enes.dietapp.fragments.HealthFragment;
+import com.example.enes.dietapp.fragments.HomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener);
+
+        FragmentTransaction homeTransaction = getSupportFragmentManager().beginTransaction();
+        homeTransaction.replace(R.id.frame_layout,new HomeFragment());
+        homeTransaction.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            if(menuItem.getItemId() == R.id.navigation_home){
+                selectedFragment = new HomeFragment();
+
+            }else if(menuItem.getItemId() == R.id.navigation_health){
+                selectedFragment = new HealthFragment();
+            }else if(menuItem.getItemId() == R.id.navigation_diet){
+                selectedFragment = new DietFragment();
+            }
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout,selectedFragment);
+            transaction.commit();
+
+            return true;
+        }
+    };
+}
